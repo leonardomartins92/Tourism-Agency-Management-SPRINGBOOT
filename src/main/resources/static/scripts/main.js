603f6d82d1ec1d8a454c31c1011a0654838e5542
Leonardo Rodrigues <59699766+leonardomartins92@users.noreply.github.com>
2021-04-03 19:26:46 -0300

    Tratamento de datas

--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -15,7 +15,6 @@ var getUrlParameter = function getUrlParameter(sParam) {
 };
 
 var action = getUrlParameter('operacao');
-console.log(action);
 
 if(action === "Excluir"){
   if(document.getElementsByTagName("input") !== null){
@@ -31,4 +30,21 @@ if(action === "Excluir"){
     }
   }
 }
+
+function verificaData(){
+   var ida = document.getElementById("ida").value;
+   var volta = document.getElementById("volta").value;
+   var msg =  document.getElementById("msgErro");
+   var btn =  document.getElementById("botaoSubmit");
+
+   if((volta != "") && (volta < ida)){
+     btn.disabled = true;
+     msg.innerText ="Data de Volta deve ser posterior à Data de Ida";
+   }
+   else{
+     btn.disabled = false;
+     msg.innerText = "";
+   }
+}
+
 
